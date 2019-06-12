989ebd837580a3d241cda36a9eab798a414f2ff6
XiangLei1996 <xiang9286@foxmail.com>
2019-06-12 18:47:28 +0800

    更新通用返回对象和异常处理功能

---
+++ src/main/java/com/miaoshaproject/error/EmBusinessError.java
@@ -0,0 +1,52 @@
+package com.miaoshaproject.error;
+
+/**
+ * Author: XiangL
+ * Date: 2019/6/12 17:34
+ * Version 1.0
+ *
+ * 定义枚举类，用来判断错误属于那种类型
+ * 类中通过枚举指定多种不同类型的参数
+ *
+ * 注意：枚举类型之间使用 逗号 相分隔
+ */
+public enum EmBusinessError implements CommonError {
+
+    //通用错误类型10001
+    PARAMETER_VALIDATION_ERROR(10001, "参数不合法"),
+
+    //未知错误10002
+    UNKNOW_ERROR(10002, "未知错误"),
+
+    //20000开头为用户信息相关错误定义
+    USER_NOT_EXIST(20001, "用户不存在");
+
+    private EmBusinessError(int errCode, String errMsg){
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    private int errCode;
+    private String errMsg;
+
+    @Override
+    public int getErrCode() {
+        return 0;
+    }
+
+    @Override
+    public String getErrMsg() {
+        return null;
+    }
+
+    /**
+     * 用于可以自己传入参数来覆盖原有errMsg，自己设定errMsg
+     * @param errMsg
+     * @return
+     */
+    @Override
+    public CommonError setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+        return this;
+    }
+}
