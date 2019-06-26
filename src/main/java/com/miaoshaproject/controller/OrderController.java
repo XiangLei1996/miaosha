4df87b373b70c09a7c7137db7dc3a249beef1606
XiangLei1996 <xiang9286@foxmail.com>
2019-06-26 18:58:35 +0800

    引入秒杀令牌进行流量控制，使用线程池实现本地队列泄洪 添加了生成验证码图片的CodeUtil类

--- src/main/java/com/miaoshaproject/controller/OrderController.java
+++ src/main/java/com/miaoshaproject/controller/OrderController.java
@@ -7,13 +7,22 @@ import com.miaoshaproject.mq.MqProducer;
 import com.miaoshaproject.respones.CommonReturnType;
 import com.miaoshaproject.service.ItemService;
 import com.miaoshaproject.service.OrderService;
+import com.miaoshaproject.service.PromoService;
+import com.miaoshaproject.util.CodeUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.RenderedImage;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.*;
 
 /**
  * Author: XiangL
@@ -42,21 +51,49 @@ public class OrderController extends BaseController{
     @Autowired
     private ItemService itemService;
 
-    //封装下单请求
-    @RequestMapping(path = {"/createorder"}, method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
-    @ResponseBody
-    public CommonReturnType createOrder(@RequestParam("itemId") Integer itemId,
-                                        @RequestParam("amount") Integer amount,
-                                        @RequestParam(value = "promoId", required = false) Integer promoId) throws BusinessException {
-        //判断用户是否登录，使用token方法后注释
-        //Boolean isLogin = (Boolean)httpServletRequest.getSession().getAttribute("IS_LOGIN");
-//        if(isLogin == null || !isLogin.booleanValue()){
-//            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "请先登录用户");
-//        }
-        //获取用户登录信息--注意需要强转类型---值是在用户登录存储在对应的request的session里的
-        //UserModel userModel = (UserModel) httpServletRequest.getSession().getAttribute("LOGIN_USER");
+    @Autowired
+    private PromoService promoService;
 
-        //使用token验证登录
+    //用于队列泄洪;利用了线程池实现
+    private ExecutorService executorService;
+
+    //要知道@PostConstruct的作用
+    @PostConstruct
+    public void init(){
+        //生成固定大小的线程池
+        executorService = Executors.newFixedThreadPool(20);
+    }
+
+
+    //生成验证码
+    @RequestMapping(path = {"/generateverifycode"}, method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public void generateVerifyCode(HttpServletResponse response) throws BusinessException, IOException {
+        String token = httpServletRequest.getParameterMap().get("token")[0];
+        if(StringUtils.isEmpty(token)){
+            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN,"用户还未登录，不能生成验证码");
+        }
+
+        UserModel userModel = (UserModel) redisTemplate.opsForValue().get(token);
+        if(userModel == null){
+            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "请先登录用户");
+        }
+
+        Map<String, Object> map = CodeUtil.generateCodeAndPic();
+        //并将该验证码与用户做一个绑定
+        redisTemplate.opsForValue().set("verify_code_"+userModel.getId(), map.get("code"));
+        //将生成的图片写到httpServletResponse中，返回给前端
+        ImageIO.write((RenderedImage) map.get("codePic"), "jpeg", response.getOutputStream());
+    }
+
+
+    //生成秒杀令牌
+    @RequestMapping(path = {"/generatetoken"}, method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType generatetoken(@RequestParam("itemId") Integer itemId,
+                                          @RequestParam("promoId") Integer promoId,
+                                          @RequestParam("verifyCode") String verifyCode) throws BusinessException{
+        //根据token获取用户信息
         String token = httpServletRequest.getParameterMap().get("token")[0];
         if(StringUtils.isEmpty(token)){
             throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "请先登录用户");
@@ -67,20 +104,86 @@ public class OrderController extends BaseController{
             throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "请先登录用户");
         }
 
+        //获取秒杀访问令牌
+        String promoToken = promoService.generateSecondKillToken(promoId, itemId, userModel.getId());
+
+        if(promoToken == null){
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "生成令牌失败");
+        }
+
+        //返回对应的结果
+        return CommonReturnType.create(promoToken);
+    }
+
+
+    //封装下单请求
+    @RequestMapping(path = {"/createorder"}, method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType createOrder(@RequestParam("itemId") Integer itemId,
+                                        @RequestParam("amount") Integer amount,
+                                        @RequestParam(value = "promoId", required = false) Integer promoId,
+                                        @RequestParam(value = "promoToken", required = false) String promoToken) throws BusinessException {
+        //判断用户是否登录，使用token方法后注释
+        //Boolean isLogin = (Boolean)httpServletRequest.getSession().getAttribute("IS_LOGIN");
+//        if(isLogin == null || !isLogin.booleanValue()){
+//            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "请先登录用户");
+//        }
+        //获取用户登录信息--注意需要强转类型---值是在用户登录存储在对应的request的session里的
+        //UserModel userModel = (UserModel) httpServletRequest.getSession().getAttribute("LOGIN_USER");
+
+        String token = httpServletRequest.getParameterMap().get("token")[0];
+        if(StringUtils.isEmpty(token)){
+            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "请先登录用户");
+        }
+        //获取用户登录信息也通过token去redis中寻找
+        UserModel userModel = (UserModel) redisTemplate.opsForValue().get(token);
+        if(userModel == null){
+            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "请先登录用户");
+        }
+
+
+        //校验秒杀令牌是否正确
+        if(promoId != null){
+            String inRedisPromoToken = (String) redisTemplate.opsForValue().get("promo_token_"+promoId+"_userid_"+userModel.getId()+"_itemid_"+itemId);
+            if(inRedisPromoToken == null){
+                throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "秒杀令牌校验失败");
+            }
+            if(!org.apache.commons.lang3.StringUtils.equals(promoToken, inRedisPromoToken)){
+                throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR, "秒杀令牌校验失败");
+            }
+        }
+
         //此时使用异步发送事务型消息，不通过下面的普通流程创建订单
         //OrderModel orderModel = orderService.createOrder(userModel.getId(), itemId, amount, promoId);
 
-        //通过库存售罄标识是否存在，来判断库存是否已售罄，售罄则直接返回失败
-        if(redisTemplate.hasKey("promo_item_stock_invalid_"+itemId)){
-            throw new BusinessException(EmBusinessError.STOCK_NOT_ENOUGH);
-        }
 
-        //先加入库存流水init状态，然后再去完成对应的下单事务型消息机制
-        String stockLogId = itemService.initStockLog(itemId, amount);
+        //同步调用线程池的submit方法；使用实现Callable接口重写call方法的方式创建线程
+        //第三种方法创建的线程，存在返回值。必须使用Future进行包装
 
-        //这里需要false才返回下单失败
-        if(!mqProducer.transactionAsyncReduceStock(userModel.getId(), itemId, amount, promoId, stockLogId)){
-            throw new BusinessException(EmBusinessError.UNKNOW_ERROR, "下单失败");
+        //拥塞窗口为20的等待队列，用来队列泄洪
+        Future<Object> future = executorService.submit(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+
+                //先加入库存流水init状态，然后再去完成对应的下单事务型消息机制
+                String stockLogId = itemService.initStockLog(itemId, amount);
+
+                //这里需要false才返回下单失败
+                if(!mqProducer.transactionAsyncReduceStock(userModel.getId(), itemId, amount, promoId, stockLogId)){
+                    throw new BusinessException(EmBusinessError.UNKNOW_ERROR, "下单失败");
+                }
+
+                return null;
+            }
+        });
+
+        try {
+            //等待返回，返回后才会执行后序给前端返回信息
+            future.get();
+        } catch (InterruptedException e) {
+            throw new BusinessException(EmBusinessError.UNKNOW_ERROR);
+        } catch (ExecutionException e) {
+            throw new BusinessException(EmBusinessError.UNKNOW_ERROR);
         }
 
         return CommonReturnType.create(null);
